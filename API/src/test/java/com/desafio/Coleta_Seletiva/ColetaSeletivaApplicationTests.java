39099a84b53c9b88b07c5553bc36ff08a21ddad7
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-11-09 12:18:09 -0300

    close #19, chore: add and configure spring dep

---
+++ API/src/test/java/com/desafio/Coleta_Seletiva/ColetaSeletivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.Coleta_Seletiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColetaSeletivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
