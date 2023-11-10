003e78649e01a261a8bb77c5099fcf2827ec455f
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-11-10 11:55:00 -0300

    #32 add toDTO method in material mapper

--- API/src/main/java/com/desafio/Coleta_Seletiva/dto/Material/mapper/MaterialMapper.java
+++ API/src/main/java/com/desafio/Coleta_Seletiva/dto/Material/mapper/MaterialMapper.java
@@ -3,10 +3,15 @@ package com.desafio.Coleta_Seletiva.dto.Material.mapper;
 import org.modelmapper.ModelMapper;
 
 import com.desafio.Coleta_Seletiva.dto.Material.MaterialCreateDTO;
+import com.desafio.Coleta_Seletiva.dto.Material.MaterialResponseDTO;
 import com.desafio.Coleta_Seletiva.entities.Material.Material;
 
 public class MaterialMapper {
   public static Material toMaterial(MaterialCreateDTO createDTO) {
     return new ModelMapper().map(createDTO, Material.class);
+  }
+
+  public static MaterialResponseDTO toDTO(Material material) {
+    return new ModelMapper().map(material, MaterialResponseDTO.class);
   }
 }
