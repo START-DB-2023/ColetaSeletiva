bf8064b800af197d16800526c4dc6a6d626e4aa2
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-11-10 10:11:18 -0300

    #31 add Material RestController

---
+++ API/src/main/java/com/desafio/Coleta_Seletiva/controllers/MaterialController.java
@@ -0,0 +1,24 @@
+package com.desafio.Coleta_Seletiva.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desafio.Coleta_Seletiva.entities.Material.Material;
+import com.desafio.Coleta_Seletiva.services.MaterialService;
+
+@RestController
+@RequestMapping("api/materiais")
+public class MaterialController {
+  @Autowired
+  private MaterialService materialService;
+
+  public MaterialController(MaterialService materialService) {
+    this.materialService = materialService;
+  }
+}
