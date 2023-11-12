68ede55e8c7f0a6eb58109b1edd374b14f39ca3b
Aurora Kruschewsky <auro.kruschewsky@gmail.com>
2023-11-12 10:37:45 -0300

    FrontEnd Structure

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <h1>Em Desenvolvimento!</h1>
+        <h3>Por favor volte outro dia.</h3>
+        <img className='inDevelopment' src='./public/in-development.png' alt='Imagem de Desenvolvimento'></img>
+      </div>
+    </>
+  )
+}
+
+export default App
