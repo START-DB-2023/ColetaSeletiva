50991e0ec7b5ad4fc70f095efff6fc1fa4c8ac0a
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-11-13 11:55:03 -0300

    close #40, chore: api exception handler

---
+++ API/src/main/java/com/desafio/Coleta_Seletiva/web/exception/APIExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.desafio.Coleta_Seletiva.web.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class APIExceptionHandler {
+  private static final Logger log = LoggerFactory.getLogger(APIExceptionHandler.class);
+
+}
