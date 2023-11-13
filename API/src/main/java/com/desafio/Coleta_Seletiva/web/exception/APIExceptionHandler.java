d510c5b841d17dfc92929dbd9f5e00d443e609c3
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-11-13 17:04:45 -0300

    #40 add mmethod to handle not valid arguments

--- API/src/main/java/com/desafio/Coleta_Seletiva/web/exception/APIExceptionHandler.java
+++ API/src/main/java/com/desafio/Coleta_Seletiva/web/exception/APIExceptionHandler.java
@@ -2,10 +2,26 @@ package com.desafio.Coleta_Seletiva.web.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class APIExceptionHandler {
   private static final Logger log = LoggerFactory.getLogger(APIExceptionHandler.class);
 
+  // Lida com exceções de campos inválidos nas requisições
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorMessage> methodArgumentsNotValidException(MethodArgumentNotValidException exception,
+      HttpServletRequest request, BindingResult result) {
+    log.error("API Error - ", exception);
+    return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).contentType(MediaType.APPLICATION_JSON)
+        .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campo(s) invalido(s)", result));
+  }
 }
