dc423b669cbb6877c56c4a4a0c9a62ad110856e3
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-11-13 10:42:24 -0300

    #36 func: material delete resource

--- API/src/main/java/com/desafio/Coleta_Seletiva/controllers/MaterialController.java
+++ API/src/main/java/com/desafio/Coleta_Seletiva/controllers/MaterialController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +45,12 @@ public class MaterialController {
   public ResponseEntity<MaterialResponseDTO> findById(@PathVariable long id) {
     Material material = materialService.findById(id);
     return ResponseEntity.ok().body(MaterialMapper.toDTO(material));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable long id) {
+    materialService.delete(id);
+    return ResponseEntity.noContent().build();
   }
 
 }
