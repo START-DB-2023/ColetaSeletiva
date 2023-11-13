b0b5c39979b162a2485360cf80fe2144714c6ef3
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-11-13 17:03:29 -0300

    #40 add entity not found exception

---
+++ API/src/main/java/com/desafio/Coleta_Seletiva/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.desafio.Coleta_Seletiva.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+  public EntityNotFoundException(String message) {
+    super(message);
+  }
+}
