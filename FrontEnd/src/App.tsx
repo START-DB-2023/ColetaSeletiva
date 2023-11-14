0d50f399012f439021ea494f6e19498e16af5789
Aurora Kruschewsky <auro.kruschewsky@gmail.com>
2023-11-14 12:32:32 -0300

    close #48, Material Forms page Created

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,14 +1,11 @@
+import { RegisterMaterial } from './components/Forms/RegisterMaterial'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div className='container'>
-        <h1>Em Desenvolvimento!</h1>
-        <h3>Por favor volte outro dia.</h3>
-        <img className='inDevelopment' src='./public/in-development.png' alt='Imagem de Desenvolvimento'></img>
-      </div>
+      <RegisterMaterial />
     </>
   )
 }
---
+++ FrontEnd/src/components/pageHome/PageHome.tsx
@@ -0,0 +1,7 @@
+export function pageHome() {
+
+    return (
+        <>
+        </>
+    );
+}
