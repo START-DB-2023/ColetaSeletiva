048a83af934d16720892e58ff176b78742c9066a
jade-vacineski <ajadne856@gmail.com>
2023-11-14 20:24:34 -0300

    AdministradoraDTO created

--- API/src/main/java/com/desafio/Coleta_Seletiva/administradora/services/AdministradoraService.java
+++ API/src/main/java/com/desafio/Coleta_Seletiva/administradora/services/AdministradoraService.java
@@ -1,9 +1,7 @@
 package com.desafio.Coleta_Seletiva.administradora.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 import com.desafio.Coleta_Seletiva.administradora.model.Administradora;
 import com.desafio.Coleta_Seletiva.administradora.repositories.AdministradoraRepository;
 import com.desafio.Coleta_Seletiva.administradora.services.exception.AdministradoraNotFoundException;
