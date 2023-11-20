51f33d1f93e83b63789fd7e3abcf72caeb6c60da
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-11-20 15:05:42 -0300

    #66  update FormsMaterial to make request

--- FrontEnd/src/components/index.tsx
+++ FrontEnd/src/components/index.tsx
@@ -1,5 +1,5 @@
 import Material from "./Material/Material";
 import List from "./List/List";
 import Title from "./Title/Title";
-
-export { Material, List, Title };
+import FormsMaterial from "./FormsMaterial/FormsMaterial";
+export { Material, List, Title, FormsMaterial };
