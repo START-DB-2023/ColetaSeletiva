b3e6345165fc840f5a0be1aecb10fb17cf81e8b4
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-11-20 15:04:28 -0300

    #66 add page to create new material

---
+++ FrontEnd/src/pages/NewMaterialPage/NewMaterialPage.tsx
@@ -0,0 +1,14 @@
+import { FormsMaterial, Title } from "../../components";
+
+function NewMaterialPage() {
+  return (
+    <>
+      <main>
+        <Title content="Novo material"></Title>
+        <FormsMaterial></FormsMaterial>
+      </main>
+    </>
+  );
+}
+
+export default NewMaterialPage;
