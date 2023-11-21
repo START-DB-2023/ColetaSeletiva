2876424c174fb1472c2c8dda575c8a661571a317
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-11-21 16:31:34 -0300

    close #73, improve 404 page

--- FrontEnd/src/pages/ErrorPage/ErrorPage.tsx
+++ FrontEnd/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
-import './ErrorPage.css'
+import "./ErrorPage.css";
+import { Button } from "../../components";
 
 function ErrorPage() {
   return (
-    <section>
-      <h1>404</h1>
-      <h2>Desculpe, essa página não pode ser encontrada.</h2>
-      <Link to={"/"}>Início</Link>
+    <section className="c-error-page u-padding">
+      <header className="c-error-page__header">
+        <h1>404</h1>
+        <h2>Desculpe, essa página não pode ser encontrada.</h2>
+        <Button color={"blue"} textColor="#fff">
+          <Link to={"/"}>Início</Link>
+        </Button>
+      </header>
+
+      <img src="/404/404.png" alt="" />
     </section>
   );
 }
