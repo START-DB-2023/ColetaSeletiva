840eda83553f5e7455c46d3e4cca7153386952c6
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-11-21 16:28:54 -0300

    improve Button component

--- FrontEnd/src/components/Button/Button.tsx
+++ FrontEnd/src/components/Button/Button.tsx
@@ -1,9 +1,21 @@
 import { ReactElement } from "react";
 import "./Button.css";
 
-function Button(props: { color: string; children: ReactElement | string }) {
+function Button(props: {
+  color?: string;
+  textColor?: string;
+  fontSize?: string;
+  children: ReactElement | string;
+}) {
   return (
-    <button className="c-button" style={{ background: props.color }}>
+    <button
+      className="c-button"
+      style={{
+        background: props.color,
+        color: props.textColor,
+        fontSize: props.fontSize,
+      }}
+    >
       {props.children}
     </button>
   );
